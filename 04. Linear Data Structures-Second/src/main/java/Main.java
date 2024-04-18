033cd36f43e9549c2bcdaff022e559a87cc53a8f
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-04-18 23:28:58 +0300

    Update Main.java

--- 04. Linear Data Structures-Second/src/main/java/Main.java
+++ 04. Linear Data Structures-Second/src/main/java/Main.java
@@ -1,5 +1,20 @@
+import implementations.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList testlist = new ArrayList<>();
+        testlist.add(1);
+        testlist.add(2);
+        testlist.add(3);
+        testlist.add(4);
+        testlist.add(4);
+        testlist.add(4);
+        System.out.println(testlist.get(0));
+        System.out.println(testlist.get(1));
+        testlist.add(0,9);
+        System.out.println(testlist.get(0));
+        System.out.println(testlist.get(1));
+//        System.out.println(testlist.get(4));
 
     }
 }
