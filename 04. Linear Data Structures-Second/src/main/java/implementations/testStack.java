0ae4f451a1e5bf1d4c916314d6d00eb3f1a59a82
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-04-25 23:07:24 +0300

    Create testStack.java

---
+++ 04. Linear Data Structures-Second/src/main/java/implementations/testStack.java
@@ -0,0 +1,83 @@
+package implementations;
+
+import interfaces.AbstractStack;
+
+import java.util.Iterator;
+
+
+public class testStack<E> implements AbstractStack<E> {
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.value = element;
+        }
+    }
+
+    public testStack() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void push(E element) {
+        Node<E> elementToInsert = new Node<>(element);
+        elementToInsert.next = this.top;
+        this.top = elementToInsert;
+        this.size++;
+    }
+
+    @Override
+    public E pop() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        Node<E> tmp = this.top;
+        this.top = tmp.next;
+        this.size--;
+        return tmp.value;
+    }
+
+    @Override
+    public E peek() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return this.top.value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+
+        return new Iterator<E>() {
+            private Node<E> current = top;
+            @Override
+            public boolean hasNext() {
+                return this.current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = this.current.value;
+                this.current = this.current.next;
+                return value;
+            }
+        };
+    }
+}
