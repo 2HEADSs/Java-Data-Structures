ff278e138cc0f822710bbab6adae832542f343b3
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-04-26 23:39:43 +0300

    Update Stack.java

--- 04. Linear Data Structures-Second/src/main/java/implementations/Stack.java
+++ 04. Linear Data Structures-Second/src/main/java/implementations/Stack.java
@@ -5,30 +5,60 @@ import interfaces.AbstractStack;
 import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
 
     @Override
     public void push(E element) {
-
+        Node<E> newTop = new Node<>(element);
+        newTop.next = this.top;
+        this.top = newTop;
+        this.size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        Node<E> currentTop = this.top;
+        this.top = currentTop.next;
+        this.size--;
+        return currentTop.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return this.top.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
