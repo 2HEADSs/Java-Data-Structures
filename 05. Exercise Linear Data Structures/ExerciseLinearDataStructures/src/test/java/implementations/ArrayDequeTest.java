c64e27393c35c201aad15502546b479072fc8e83
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-01 15:58:42 +0300

    Update ArrayDequeTest.java

--- 05. Exercise Linear Data Structures/ExerciseLinearDataStructures/src/test/java/implementations/ArrayDequeTest.java
+++ 05. Exercise Linear Data Structures/ExerciseLinearDataStructures/src/test/java/implementations/ArrayDequeTest.java
@@ -1,7 +1,18 @@
 package implementations;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ArrayDequeTest {
 
+
+    @Test
+    public  void  testArrayDeque(){
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(13);
+        deque.add(14);
+        deque.add(15);
+
+    }
 }
