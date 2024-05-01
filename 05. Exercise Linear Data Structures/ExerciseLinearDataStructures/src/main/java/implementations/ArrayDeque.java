492a70dea4b8b987ccc3f531f09fbc5dd2ad52e9
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-01 14:51:41 +0300

    Update ArrayDeque.java

--- 05. Exercise Linear Data Structures/ExerciseLinearDataStructures/src/main/java/implementations/ArrayDeque.java
+++ 05. Exercise Linear Data Structures/ExerciseLinearDataStructures/src/main/java/implementations/ArrayDeque.java
@@ -4,18 +4,51 @@ import interfaces.Deque;
 
 import java.util.Iterator;
 
-public class  ArrayDeque<E> implements Deque<E> {
+public class ArrayDeque<E> implements Deque<E> {
     private final int INITIAL_CAPACITY = 7;
     private int size;
+    private int head;
+    private int tail;
     private Object[] elements;
-    @Override
-    public void add(E Element) {
 
+    public ArrayDeque() {
+        this.elements = new Object[INITIAL_CAPACITY];
+        int middle = INITIAL_CAPACITY / 2;
+        head = tail = middle;
+    }
+
+    @Override
+    public void add(E element) {
+        if (this.size == 0) {
+            this.elements[this.tail] = element;
+        } else {
+            if (this.tail == this.elements.length - 1) {
+                this.elements = grow();
+            }
+            this.elements[++this.tail] = element;
+        }
+        this.size++;
+    }
+
+    private Object[] grow() {
+        int newCapacity = this.elements.length * 2 + 1;
+        Object[] newElements = new Object[newCapacity];
+
+        int middle = newCapacity / 2;
+        int begin = middle - this.size / 2;
+        int index = this.head;
+        for (int i = begin; i <= this.tail; i++) {
+            newElements[i] = this.elements[index];
+        }
+        this.head = begin;
+        this.tail = this.head + this.size - 1;
+
+        return newElements;
     }
 
     @Override
     public void offer(E element) {
-
+        add(element);
     }
 
     @Override
