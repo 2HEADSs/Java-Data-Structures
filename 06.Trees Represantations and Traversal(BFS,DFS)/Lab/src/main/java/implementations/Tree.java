d72d25a6856ead7383390a39ab87c2933a76c243
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-05 14:35:29 +0300

    Update Tree.java

--- 06.Trees Represantations and Traversal(BFS,DFS)/Lab/src/main/java/implementations/Tree.java
+++ 06.Trees Represantations and Traversal(BFS,DFS)/Lab/src/main/java/implementations/Tree.java
@@ -2,9 +2,7 @@ package implementations;
 
 import interfaces.AbstractTree;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Tree<E> implements AbstractTree<E> {
     private E value;
@@ -21,7 +19,20 @@ public class Tree<E> implements AbstractTree<E> {
 
     @Override
     public List<E> orderBfs() {
-        return null;
+        List<E> result = new ArrayList<>();
+        Deque<Tree<E>> childrenQueue = new ArrayDeque<>();
+        childrenQueue.offer(this);
+
+        while (!childrenQueue.isEmpty()){
+            Tree<E> current = childrenQueue.poll();
+
+            result.add(current.value);
+
+            for (Tree<E> child : current.children) {
+                childrenQueue.offer(child);
+            }
+        }
+        return result;
     }
 
     @Override
