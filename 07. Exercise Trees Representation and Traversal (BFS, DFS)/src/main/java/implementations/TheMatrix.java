61ff073d3e3ed0c6d4ea0652c9f30ef1a9f0b672
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-11 22:28:06 +0300

    Update TheMatrix.java

--- 07. Exercise Trees Representation and Traversal (BFS, DFS)/src/main/java/implementations/TheMatrix.java
+++ 07. Exercise Trees Representation and Traversal (BFS, DFS)/src/main/java/implementations/TheMatrix.java
@@ -1,5 +1,8 @@
 package implementations;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class TheMatrix {
     private char[][] matrix;
     private char fillChar;
@@ -16,22 +19,35 @@ public class TheMatrix {
     }
 
     public void solve() {
-        fillMatrix(this.startRow, this.startCol);
-    }
-
-    private void fillMatrix(int row, int col) {
-        if (isOutOfBounds(row, col) || this.matrix[row][col] != this.startChar) {
-            return;
+        Deque<int[]> coordinates = new ArrayDeque<>();
+        coordinates.offer(new int[]{startRow, startCol});
+        while (!coordinates.isEmpty()) {
+            int[] position = coordinates.poll();
+            int row = position[0];
+            int col = position[1];
+            this.matrix[row][col] = this.fillChar;
+            if (isInBounds(row + 1, col) && this.matrix[row + 1][col] == this.startChar) {
+                coordinates.offer(new int[]{row + 1, col});
+            }
         }
-        this.matrix[row][col] = this.fillChar;
-
-        System.out.println(this.toOutputString());
-
-        this.fillMatrix(row + 1, col);
-        this.fillMatrix(row, col + 1);
-        this.fillMatrix(row - 1, col);
-        this.fillMatrix(row, col - 1);
+//        fillMatrix(this.startRow, this.startCol);
     }
+
+
+
+//    private void fillMatrix(int row, int col) {
+//        if (isOutOfBounds(row, col) || this.matrix[row][col] != this.startChar) {
+//            return;
+//        }
+//        this.matrix[row][col] = this.fillChar;
+//
+//        System.out.println(this.toOutputString());
+//
+//        this.fillMatrix(row + 1, col);
+//        this.fillMatrix(row, col + 1);
+//        this.fillMatrix(row - 1, col);
+//        this.fillMatrix(row, col - 1);
+//    }
 
     private boolean isOutOfBounds(int row, int col) {
         return row < 0 || row >= this.matrix.length || col < 0 || col >= this.matrix[row].length;
