1ab8fd942ef3a0ee4a2f5f43b8705a694c845c68
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-17 23:52:17 +0300

    Update BinaryTree.java

--- 08. Heaps, BST/src/main/java/implementations/BinaryTree.java
+++ 08. Heaps, BST/src/main/java/implementations/BinaryTree.java
@@ -7,14 +7,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class BinaryTree<E> implements AbstractBinaryTree<E> {
-    public int size;
 
-    public BinaryTree() {
-    }
-
-    public BinaryTree(int size) {
-        this.size = size;
-    }
 
     @Override
     public E getKey() {
