68d91054f99442058d57ea7ee6636a5d93e1d856
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-19 11:36:43 +0300

    Update PriorityQueue.java

--- 08. Heaps, BST/src/main/java/implementations/PriorityQueue.java
+++ 08. Heaps, BST/src/main/java/implementations/PriorityQueue.java
@@ -8,23 +8,79 @@ import java.util.List;
 
 public class PriorityQueue<E extends Comparable<E>> implements AbstractQueue<E> {
 
+    private List<E> elements;
+
+    public PriorityQueue() {
+        this.elements = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.hipifyUp(this.size() - 1);
+    }
 
+    private void hipifyUp(int index) {
+        while (index > 0 && isLess(getParentIndex(index), index)) {
+            Collections.swap(this.elements, getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    private boolean isLess(int parentIndex, int childIndex) {
+        return getAt(parentIndex).compareTo(getAt(childIndex)) < 0;
+    }
+
+    private E getAt(int index) {
+        return this.elements.get(index);
+    }
+
+    private int getParentIndex(int index) {
+        return (index - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+        return this.elements.get(0);
+    }
+
+    private void ensureNonEmpty() {
+        if (this.size() == 0) {
+            throw new IllegalStateException("Illegal call to peak on empty heap");
+        }
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureNonEmpty();
+        E returnedValue = getAt(0);
+        Collections.swap(this.elements, 0, this.size() - 1);
+        this.elements.remove(this.size() - 1);
+        this.heapifyDown(0);
+        return returnedValue;
+    }
+
+    private E getLeftChild(int index) {
+
+
+    }
+
+    private int getLeftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    private int getRightChildIndex(int index) {
+        return 2 * index + 2;
+
+    }
+
+    private void heapifyDown(int i) {
+
     }
 }
