9b8c2d24d4bee4508cb0d92ee549c08ba7498fd4
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-19 12:17:13 +0300

    Update BinarySearchTree.java

--- 08. Heaps, BST/src/main/java/implementations/BinarySearchTree.java
+++ 08. Heaps, BST/src/main/java/implementations/BinarySearchTree.java
@@ -12,7 +12,38 @@ public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinary
         Node<E> newNode = new Node<>(element);
         if (this.getRoot() == null) {
             this.root = newNode;
+        } else {
+            Node<E> current = this.root;
+            Node<E> prev = current;
+            while (current != null) {
+                prev = current;
+                if (isLess(element, current.value)) {
+                    current = current.leftChild;
+                } else if (isGreater(element, current.value)) {
+                    current = current.rightChild;
+                } else if (areEqual(element, current.value)) {
+                    return;
+                }
+            }
+
+            if (isLess(element, prev.value)) {
+                prev.leftChild = newNode;
+            } else if (isGreater(element, prev.value)) {
+                prev.rightChild = newNode;
+            }
         }
+    }
+
+    private boolean isLess(E first, E second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private boolean isGreater(E first, E second) {
+        return first.compareTo(second) > 0;
+    }
+
+    private boolean areEqual(E first, E second) {
+        return first.compareTo(second) == 0;
     }
 
     @Override
