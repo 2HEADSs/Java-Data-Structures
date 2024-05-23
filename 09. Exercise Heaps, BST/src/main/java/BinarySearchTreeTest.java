57ec080e9af04e4d7de6ef04bfe572efcbba98f8
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-23 22:47:59 +0300

    Update BinarySearchTreeTest.java

--- 09. Exercise Heaps, BST/src/main/java/BinarySearchTreeTest.java
+++ 09. Exercise Heaps, BST/src/main/java/BinarySearchTreeTest.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 public class BinarySearchTreeTest {
     @Test
     public void testCreate(){
-        BinarySearchTree<Integer> bst = new BinarySearchTree();
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>(5);
+        assertEquals(Integer.valueOf(5), bst.getRoot().getValue());
+
+
     }
 
 }
