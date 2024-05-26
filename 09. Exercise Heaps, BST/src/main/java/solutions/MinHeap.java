19d375f96f91746c2c9d6cc1778ba8146d874acd
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-26 11:23:56 +0300

    Update MinHeap.java

--- 09. Exercise Heaps, BST/src/main/java/solutions/MinHeap.java
+++ 09. Exercise Heaps, BST/src/main/java/solutions/MinHeap.java
@@ -3,21 +3,42 @@ package solutions;
 import interfaces.Decrease;
 import interfaces.Heap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinHeap<E extends Comparable<E> & Decrease<E>> implements Heap<E> {
+    private List<E> data;
+
+    public MinHeap() {
+        this.data = new ArrayList<>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.data.size();
     }
 
     @Override
     public void add(E element) {
+        this.data.add(element);
+        this.heapifyUp();
+    }
 
+    private void heapifyUp() {
+        int index = this.data.size() - 1;
+        int parentIndex = (index - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+        return this.data.get(0);
+    }
+
+    private void ensureNonEmpty() {
+        if (this.data.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 
     @Override
