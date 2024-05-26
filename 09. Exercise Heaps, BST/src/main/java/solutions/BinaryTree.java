225ad682a8a424cf0fde2fa55e442a5af40cac3e
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-26 10:34:58 +0300

    Update BinaryTree.java

--- 09. Exercise Heaps, BST/src/main/java/solutions/BinaryTree.java
+++ 09. Exercise Heaps, BST/src/main/java/solutions/BinaryTree.java
@@ -3,9 +3,14 @@ package solutions;
 import java.util.List;
 
 public class BinaryTree {
+    private int value;
+    private BinaryTree left;
+    private BinaryTree right;
 
     public BinaryTree(int key, BinaryTree first, BinaryTree second) {
-
+        this.value = key;
+        this.left = first;
+        this.right = second;
     }
 
     public Integer findLowestCommonAncestor(int first, int second) {
