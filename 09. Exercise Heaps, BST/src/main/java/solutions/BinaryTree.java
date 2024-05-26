0fee8c0c23940087e7eeb018541dab4ccdc0f5a9
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-05-26 10:40:55 +0300

    Update BinaryTree.java

--- 09. Exercise Heaps, BST/src/main/java/solutions/BinaryTree.java
+++ 09. Exercise Heaps, BST/src/main/java/solutions/BinaryTree.java
@@ -14,7 +14,19 @@ public class BinaryTree {
     }
 
     public Integer findLowestCommonAncestor(int first, int second) {
-        return null;
+        List<Integer> firstPath = findPath(this, first);
+        List<Integer> secondPath = findPath(this, second);
+
+        int smallerSize = Math.min(firstPath.size(), secondPath.size());
+
+        int i = 0;
+        for (; i < smallerSize; i++) {
+            if (!firstPath.get(i).equals(secondPath.get(i))) {
+                break;
+            }
+        }
+        
+        return firstPath.get(i - 1);
     }
 
     public List<Integer> topView() {
